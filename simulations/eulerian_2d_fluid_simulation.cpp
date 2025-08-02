2565380fa3930bda357c6b712778982f1f9ffa82
Kevon <you@example.com>
2025-08-02 20:29:21 +0100

    cpp simulation added

---
+++ simulations/eulerian_2d_fluid_simulation.cpp
@@ -0,0 +1,14 @@
+// COMPUTATIONAL FLUID DYNAMICS
+// ARC PROJECTS - SIMULATION PROJECT 05-07-2025
+// This project is intended to demonstrate the basics with regards to computational fluid dynamics, presenting a 2-Dimensional Fluid Dynamics PDE Solver in the form of a visual simulation.
+// The learning objectives associated with this project inciude strengthening familiarity with solving differential equations using computationally efficient methods, using C++ for complex mathematical modelling, and gain a stronger understanding of the modelling process.
+
+// ASSUMPTIONS
+// Fluid is incompressible
+// Fluid is inviscid (zero viscosity)
+// 
+
+// First step, define the equations/methods required.
+// GRIDbased method
+
+// Velocity as 2d vector v = [u v]^T
