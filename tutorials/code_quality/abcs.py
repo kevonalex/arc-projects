d849f0f4b7f2c647e00b1df95424fd7bb7fd2bb7
Kevon <you@example.com>
2025-08-02 20:29:49 +0100

    quality code added

--- tutorials/code_quality/abcs.py
+++ tutorials/code_quality/abcs.py
@@ -1,1 +1,8 @@
 # Abstract Base Classes
+# Abstract Base Classes are frequently used to create interfaces that can be used when one process or function needs to satisfy multiple possible sub-processes/features
+
+from abc import ABC
+
+class PipelineStep(ABC):
+    pass
+
