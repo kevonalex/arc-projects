d310e912b4091b1d36785824ed5e9f18870a4b70
Kevon Alexander <kevonalex01@gmail.com>
2025-12-25 08:05:29 -0400

    bisection start

--- tutorials/cpp/root_finding_methods.cpp
+++ tutorials/cpp/root_finding_methods.cpp
@@ -13,6 +13,24 @@ int main () {
     std::vector<float> y_array;
 
     float y;
+    float x_t0 = 1.0;
+    float x_T = 21.0;
+    std::cout << "We are sure that the root is between " << x_t0 << " and " << x_T << ".\n" << std::endl;
+    std::cout << "We need to find the root using the bisection method, so we will need to use a while loop to find the root." << std::endl;
+    float x_mid = (x_T - x_t0)/2;
+    std::cout << "Two new intervals are: " << x_t0 << "to" << x_mid << ", and" << x_mid << "to" << x_T << std::endl;
+    while (x_t0 < x_T) {
+        y = continuous_function(x_mid);
+        if (y == 0) {
+            std::cout << "Y is 0 at x = " << x_mid << ".\n" << std::endl;
+        }
+        else if (y < 0) {
+            x_mid = (x_t0 + x_mid)/2;
+        }
+        else {
+            x_mid = (x_mid + x_T)/2;
+        }
+    }
     for (float i = -5.0; i < 21.0; i++){        
         y = continuous_function(i);
         // push units into arrays
