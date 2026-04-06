84f9399643d706f250598f11be07c114e2bd1912
Kevon Alexander <kevonalex01@gmail.com>
2026-04-06 06:45:43 -0400

    rust intro

---
+++ tutorials/rust/introProject/src/main.rs
@@ -0,0 +1,9 @@
+// i8, i16, i32, i64, i128: Signed INTEGERS
+// u8, u16, u32, u64, u128: Unsigned INTEGERS
+
+fn main() {
+    let x: i32 = -42;
+    let y: u64 = 100;
+    println!("Signed integer: {}", x);
+    println!("Unsigned integer: {}", y);
+}
